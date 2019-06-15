fb1d371bdce569d9405d7c144b4189c984731872
Alejandro Artavia <aivatradev@gmail.com>
2019-06-15 13:48:44 -0600

    first working version of sign language recognizer

--- SignLanguageRecognizer/SignLanguageRecognizer/ViewController.swift
+++ SignLanguageRecognizer/SignLanguageRecognizer/ViewController.swift
@@ -7,12 +7,56 @@
 //
 
 import UIKit
+import AVKit
+import Vision // Machine Learning Library Using To detect object
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let captureSession = AVCaptureSession()
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
+        
+        // add it to the capture session
+        let dataOutput = AVCaptureVideoDataOutput()
+        
+        // creates a custom queue
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        
+        captureSession.addOutput(dataOutput)
+        
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+//        print("camera was able to capture a frame: \(Date())")
+        
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        // model goes here!
+        guard let model = try? VNCoreMLModel(for: SignClassifier().model) else { return }
+        
+        let request = VNCoreMLRequest(model: model) { (req, err) in
+
+            guard let results = req.results as? [VNClassificationObservation] else { return }
+            guard let firstObservation = results.first else { return }
+            
+            print(firstObservation.identifier, firstObservation.confidence)
+            
+        }
+        
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
     }
 
 
